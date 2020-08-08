cd691d9e20843fd9dceaa8548a1bfaf387504880
vantage-ola <vantageola4@gmail.com>
2020-08-08 10:22:26 +0100

    Updated to save info

--- inc_exp.c
+++ inc_exp.c
@@ -1,19 +1,18 @@
     #include <stdio.h>
-	
-	//simple program to calculate income and expenditure
-	
+	#include <stdlib.h>
+
     typedef struct Revenue {
 	float inv;
 	float vol;
 	float pos;
-	float corp;            //I call the  variables for revenue
+	float corp;
 	float cha;
     } Revenue;
 
     typedef struct Expense {
 	float sal;
 	float wag;
-	float purc;                //I call varables for expenses
+	float purc;
 	float pro;
 	float com;
 	float expbi;
@@ -26,7 +25,8 @@
 
 
 
-    int main() {
+    int main() 
+{
 
 	Revenue rev;
 
@@ -34,7 +34,7 @@
 
 	year yr;
 
-        FILE *incp;
+    
 
 	printf("Enter the year for the account your calculating. : ");
 	scanf( "%d" , &yr.yrs);
@@ -74,7 +74,7 @@
 
 	printf("Expenses for the year %d\n" , yr.yrs);
 
-	printf("Type the value for each expenses spent, if none type 0.");
+	printf("Type the value for each expenses spent, if none type 0.\n");
 	printf("\n");
 
 
@@ -100,11 +100,7 @@
 	printf("-------------------");
 	printf("\n");
 
-	printf("This is the INCOME and EXPENDITURE generated in the year %d " ,
-
-		yr.yrs);
-
-
+	
 
 	float com_rev1=(rev.inv + rev.vol + rev.pos + rev.cha + rev.corp);
 
@@ -114,11 +110,53 @@
 
 	float com_inc =  (com_rev1 - com_exp1);
 
-	printf("\n");
-	printf("INCOME : $%f"  , com_inc);
-	printf("\n");
-	printf("EXPENDITURE : $%f" , com_exp1);
-	printf("\n");
+	
 
+	FILE * fnp;
 
-    }
+	fnp= fopen("income-expense.txt" , "w+");
+	
+	if (fnp == NULL)
+	{
+		printf("Error!");
+		exit(EXIT_FAILURE);
+	}
+	fprintf(fnp,"INCOME & EXPENDITURE generated in the year %d\n " ,yr.yrs);
+	fputs("---------------------------------------------------------------------\n", fnp);
+	
+	fputs("\n", fnp);
+	fputs(" EXPENDITURES       |AMOUNT($)   			| INCOMES   	|AMOUNT($)  \n", fnp);
+	
+	fputs("----------------------------------------------------------------------\n", fnp);
+	
+	fprintf(fnp," Salary             |%.2f       			| Investors 	|%.2f	\n",exp.sal, rev.inv);
+	
+	fputs("----------------------------------------------------------------------\n", fnp);
+	
+	fprintf(fnp," Wages              |%.2f       			| Volunteers	|%.2f	\n", exp.wag, rev.vol);
+	fputs("----------------------------------------------------------------------\n", fnp);
+	
+	fprintf(fnp," Purchases          |%.2f       			| Profits   	|%.2f	\n",exp.purc, rev.pos);
+	
+	fputs("----------------------------------------------------------------------\n", fnp);												
+	
+	fprintf(fnp," Provision          |%.2f       			| Corporate 	|%.2f	\n",exp.pro, rev.corp);	             	 
+	
+	fputs("----------------------------------------------------------------------\n", fnp);			
+	
+	fprintf(fnp," Communication Fees |%.2f       			| Charity		|%.2f	\n",exp.com, rev.cha);
+	
+	fputs("----------------------------------------------------------------------\n", fnp);
+	
+	fprintf(fnp," Electricity Bills  |%.2f       			|  NIL      	|NIL	\n",exp.expbi);
+	
+	fputs("----------------------------------------------------------------------\n", fnp);
+	
+	fprintf(fnp," TOTAL              |%.2f       			|           	|%.2f   \n", com_inc, com_exp1);
+
+	fclose(fnp);
+
+	printf("Saved info to income-expense.txt \n");
+	printf("Check Current Directory to access income-expense.txt.\n");
+
+}	
